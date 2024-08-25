abbf4fd6083841774265695ff1ace637df345d6c
ahmadasnawi <ahmadaries899@gmail.com>
2024-08-25 08:03:54 +0700

    task 13

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./src/css/main.css" />
+    <!-- JS -->
+    <script src="./src/js/app.js" defer></script>
+    <title>Task 13</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="wrap-nav">
+            <div class="left-nav">
+                <div class="home">Home Page <i class="fa-solid fa-arrow-right"></i></div>
+                <div class="porto-cate">
+                    <div class="wrap-porto">
+                        <span class="porto">Portofolio</span>
+                        <span class="number">190+</span>
+                    </div>
+                    <div class="wrap-cate">
+                        <span class="cate">category</span>
+                        <span class="colection">Colection</span>
+                    </div>
+                </div>
+            </div>
+            <div class="right-nav">
+                <div class="icon"><i class="fa-brands fa-facebook"></i></div>
+                <div class="icon"><i class="fa-brands fa-twitter"></i></div>
+                <div class="icon"><i class="fa-brands fa-instagram"></i></div>
+            </div>
+            <div class="menu">Menu</div>
+        </div>
+        <section class="profile">
+            <div class="left-profile">
+                <h1 class="name">Nicolas Simons</h1>
+                <div class="profile-status">
+                    <div class="left-status">
+                        <span>Suport Manager</span>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                    <div class="right-status">
+                        <h3>Art Director - Metrolab</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="right-profile">
+                <img src="./public/assets/image/nico.jpg" alt="">
+                <div class="send">Send Donation</div>
+            </div>
+        </section>
+        <section class="category">
+            <div class="header-category">
+                <div class="left-category">
+                    <span class="bubble-category">ART DIRECTOR</span> 
+                    <span class="bubble-category">12 DESEMBER</span> 
+                    <span class="bubble-category">PORTOFOLIO</span> 
+                </div>
+                <div>
+                    <span>Metrolab / 2023 creates interesting website dashboard <br> design for company artist and </span>
+                </div>
+            </div>
+            <div class="section-wrap-card">
+                <div class="wrap-card">
+                    <div class="header-card">
+                        <div class="left-top-card">Late Post</div>
+                        <span><i class="fa-solid fa-arrow-right"></i></span>
+                    </div>
+                    <div class="img-card">
+                        <img src="./public/assets/image/swag.jpg" alt="">
+                    </div>
+                    <div class="discription">
+                        <div>Design Generate</div>
+                        <div>45K views</div>
+                    </div>
+                    <div class="about-card">
+                        <p>Testing <b>populer designing tool</b> and evaluating them with Metrolab crew and artists</p>
+                    </div>
+                </div>
+                <div class="wrap-card">
+                    <div class="header-card">
+                        <div class="left-top-card">Late Post</div>
+                        <span><i class="fa-solid fa-arrow-right"></i></span>
+                    </div>
+                    <div class="img-card">
+                        <img src="./public/assets/image/swag.jpg" alt="">
+                    </div>
+                    <div class="discription">
+                        <div>Design Generate</div>
+                        <div>45K views</div>
+                    </div>
+                    <div class="about-card">
+                        <p>Testing <b>populer designing tool</b> and evaluating them with Metrolab crew and artists</p>
+                    </div>
+                </div>
+                <div class="wrap-card">
+                    <div class="header-card">
+                        <div class="left-top-card">Late Post</div>
+                        <span><i class="fa-solid fa-arrow-right"></i></span>
+                    </div>
+                    <div class="img-card">
+                        <img src="./public/assets/image/swag.jpg" alt="">
+                    </div>
+                    <div class="discription">
+                        <div>Design Generate</div>
+                        <div>45K views</div>
+                    </div>
+                    <div class="about-card">
+                        <p>Testing <b>populer designing tool</b> and evaluating them with Metrolab crew and artists</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <footer>
+        <span class="right-foooter">16 DESEMBER 2022</span>
+        <div class="wrap-bottom-footer">
+            <div class="left-footer">
+                <div>01</div>
+            </div>
+            <div class="footer-wrap-hidden">
+                <div>ALL RIGHT RESERVED 2022</div>
+                <div class="hidden-footer">
+                    <span>COOPERATION</span>
+                    <span>ENGLISH</span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i>GET STARTED</span>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
